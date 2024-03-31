608ef71103a0ee77ad9c7020a6bcb7915db43adb
Horquidia <horquidia.a.lima@gmail.com>
2024-03-31 17:20:19 -0300

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,22 @@
+import {PencilLine} from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
     return(
         <aside className={styles.sidebar}>
-            <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="profile avatar" />
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" />
 
             <div className={styles.profile}>
-                <strong>Leslie Alexander</strong>
-                <span>UI Designer</span>
+                <img className={styles.avatar} src="https://github.com/horquidia-lima.png"/>
+                <strong>Horquidia Lima</strong>
+                <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     )
