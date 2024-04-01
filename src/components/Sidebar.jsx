84a405e2b70ef04d1dc7781506724132a4dbdc19
Horquidia <horquidia.a.lima@gmail.com>
2024-04-01 18:14:48 -0300

    Componente: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import {PencilLine} from 'phosphor-react'
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar() {
     return(
@@ -7,7 +8,7 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/horquidia-lima.png"/>
+                <Avatar src="https://github.com/horquidia-lima.png"/>
                 <strong>Horquidia Lima</strong>
                 <span>Web Developer</span>
             </div>
