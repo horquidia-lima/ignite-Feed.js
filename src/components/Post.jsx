88a66f6801ccb571b680d22ea67ff268912ef714
Horquidia <horquidia.a.lima@gmail.com>
2024-04-04 20:58:26 -0300

    Estado (useState)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,8 +4,14 @@ import ptBR from 'date-fns/locale/pt-BR'
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
+import { useState } from 'react'
+
 
 export function Post({author, publishedAt, content}){
+    const [comments, setComments] = useState([
+        1,
+        2,
+    ])
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'as' HH:mm'h'", {
         locale: ptBR,
@@ -15,6 +21,12 @@ export function Post({author, publishedAt, content}){
         locale: ptBR,
         addSuffix: true,
     })
+
+    function handleCreateNewComment(){
+        event.preventDefault()
+        
+        setComments([...comments,comments.length + 1])
+    }
 
     return(
         <article className={styles.post}>
@@ -42,7 +54,7 @@ export function Post({author, publishedAt, content}){
                 })} 
             </div>
 
-            <form className={styles.comentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.comentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea
@@ -56,9 +68,11 @@ export function Post({author, publishedAt, content}){
             </form>
 
             <div className={styles.commentList}>
-                <Comment/>
-                <Comment/>
-                <Comment/>
+                {
+                    comments.map( comment =>{
+                        return <Comment/>
+                    })
+                }
             </div>
         </article>
     )
