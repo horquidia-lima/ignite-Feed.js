9323158898de31982c0691f50d774870d5706684
Horquidia <horquidia.a.lima@gmail.com>
2024-04-05 15:18:46 -0300

    Realizando likes nos comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -49,7 +49,7 @@ export function Post({author, publishedAt, content}){
         setComments(commentWithoutDeleteOne)
     }
 
-    const isNewCommentEmpty = newCommentText.length == 0
+    const isNewCommentEmpty = newCommentText.length === 0
 
     return(
         <article className={styles.post}>
