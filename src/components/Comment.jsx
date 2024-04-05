f66e28e9a3ec472acbf1f27022c7f56a56bc96a8
Horquidia <horquidia.a.lima@gmail.com>
2024-04-05 15:30:38 -0300

    Entendendo closures no React

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -11,7 +11,10 @@ export function Comment({content,onDeleteComment}){
     }
 
     function handleLikeComment(){
-        setLikeCount(likeCount + 1)
+        //setLikeCount(likeCount + 1)
+        setLikeCount((state) => {
+            return state + 1
+        })
     }
 
     return(
